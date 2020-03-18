90096ba895dff7072792f51c361496b742a208d1
David Hermosilla <dhermosilla@huenei.com>
2020-03-18 12:21:30 -0300

    .

--- src/android/HueMercadoPago.java
+++ src/android/HueMercadoPago.java
@@ -1,5 +1,6 @@
 package com.huenei.plugins.mercadopago;
 
+import android.content.Intent;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -7,26 +8,51 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.mercadopago.android.px.core.MercadoPagoCheckout;
+import com.mercadopago.android.px.model.Payment;
+import com.mercadopago.android.px.model.exceptions.MercadoPagoError;
+import okhttp3.*;
 /**
  * This class echoes a string called from JavaScript.
  */
 public class HueMercadoPago extends CordovaPlugin {
-
+    private static final int REQUEST_CODE = 1;
+    private CallbackContext callbackContext;
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
         if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            String preferenceId = args.getString(0);
+            this.coolMethod(preferenceId);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success("Plugin Android: " + message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    private void coolMethod(String preferenceId) {
+        new MercadoPagoCheckout.Builder("TEST-c98a211b-42d5-45a3-a186-e87606d62c0a", preferenceId).build()
+                .startPayment(this, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MercadoPagoCheckout.PAYMENT_RESULT_CODE) {
+                final Payment payment = (Payment) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_PAYMENT_RESULT);
+                this.callbackContext.success(payment.getPaymentStatus());
+                //Done!
+            } else if (resultCode == RESULT_CANCELED) {
+                if (data != null && data.getExtras() != null
+                        && data.getExtras().containsKey(MercadoPagoCheckout.EXTRA_ERROR)) {
+                    final MercadoPagoError mercadoPagoError =
+                            (MercadoPagoError) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_ERROR);
+                            callbackContext.error(mercadoPagoError.getMessage());
+                    //Resolve error in checkout
+                } else {
+                    //Resolve canceled checkout
+                }
+            }
         }
     }
 }
