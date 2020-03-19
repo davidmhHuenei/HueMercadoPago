b2dc0a2c219ff84865119d23752fba76b0eb316a
David Hermosilla <dhermosilla@huenei.com>
2020-03-19 17:46:15 -0300

    Fixes

--- src/android/HueMercadoPago.java
+++ src/android/HueMercadoPago.java
@@ -7,6 +7,7 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.app.Activity;
 
 import com.mercadopago.android.px.core.MercadoPagoCheckout;
 import com.mercadopago.android.px.model.Payment;
@@ -31,18 +32,17 @@ public class HueMercadoPago extends CordovaPlugin {
 
     private void coolMethod(String preferenceId) {
         new MercadoPagoCheckout.Builder("TEST-c98a211b-42d5-45a3-a186-e87606d62c0a", preferenceId).build()
-                .startPayment(this, REQUEST_CODE);
+                .startPayment(this.cordova.getActivity(), REQUEST_CODE);
     }
 
-    @Override
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE) {
             if (resultCode == MercadoPagoCheckout.PAYMENT_RESULT_CODE) {
                 final Payment payment = (Payment) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_PAYMENT_RESULT);
                 this.callbackContext.success(payment.getPaymentStatus());
                 //Done!
-            } else if (resultCode == RESULT_CANCELED) {
+            } else if (resultCode == Activity.RESULT_CANCELED) {
                 if (data != null && data.getExtras() != null
                         && data.getExtras().containsKey(MercadoPagoCheckout.EXTRA_ERROR)) {
                     final MercadoPagoError mercadoPagoError =
