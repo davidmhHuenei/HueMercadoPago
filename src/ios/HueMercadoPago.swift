323a7763e032dbae5acaecf1456e1649eccdc25e
David <david.m.hermosilla@gmail.com>
2020-03-26 15:05:37 -0300

    .

--- src/ios/HueMercadoPago.swift
+++ src/ios/HueMercadoPago.swift
@@ -6,7 +6,7 @@ import UIKit
 
 @objc(HueMercadoPago) class HueMercadoPago : CDVPlugin {
 
-    var command: CDVCommandStatus_OK?
+    var command: CDVInvokedUrlCommand?
 
     @objc(coolMethod:) // Declare your function name.
     func coolMethod(command: CDVInvokedUrlCommand) { // write the function code.
@@ -20,9 +20,9 @@ import UIKit
         //pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
         // Send the function result back to Cordova.
 
-        let preferenceId = command.arguments[0]
+        let preferenceId = command.arguments[0] as! String
         self.command = command;
-        self.runMercadoPagoCheckoutWithLifecycle(preferenceId)
+        self.runMercadoPagoCheckoutWithLifecycle(preferenceId: preferenceId)
     }
 
 
@@ -32,38 +32,38 @@ import UIKit
         preferenceId: preferenceId).setLanguage("es")
 
         // 2) Create Checkout reference
-        checkout = MercadoPagoCheckout(builder: builder)
+        let checkout = MercadoPagoCheckout(builder: builder)
         
         let storyboard: UIStoryboard = UIStoryboard(name: "HomeMP", bundle: nil)
         //let root = UIApplication.shared.keyWindow?.rootViewController
         
         let navController = storyboard.instantiateViewController(withIdentifier: "HomeMP")
-        self.present(navController, animated: true)
+        self.viewController.present(navController, animated: true)
         
         // 3) Start with your navigation controller.
-        checkout?.start(navigationController: navController as! UINavigationController, lifeCycleProtocol: self)
+        checkout.start(navigationController: navController as! UINavigationController, lifeCycleProtocol: self)
     }
 }
 
 // MARK: Optional Lifecycle protocol implementation example.
-extension ViewController: PXLifeCycleProtocol {
+extension HueMercadoPago: PXLifeCycleProtocol {
     public func finishCheckout() -> ((_ payment: PXResult?) -> Void)? {
-        self.dismiss(animated: false)
+        self.viewController.dismiss(animated: false)
         return ({ (_ payment: PXResult?) in
             let pluginResult = CDVPluginResult(
                 status: CDVCommandStatus_OK,
-                messageAs: self.payment
+                messageAs: payment as? [AnyHashable : Any]
             )
-            self.commandDelegate!.send(pluginResult, callbackId: self.command.callbackId);
+            self.commandDelegate!.send(pluginResult, callbackId: self.command?.callbackId);
         })
     }
 
     func cancelCheckout() -> (() -> Void)? {
         let pluginResult = CDVPluginResult(
                 status: CDVCommandStatus_OK,
-                messageAsString: "Canceled"
+                messageAs: "Canceled"
             )
-        self.commandDelegate!.send(pluginResult, callbackId: self.command.callbackId);
+        self.commandDelegate!.send(pluginResult, callbackId: self.command?.callbackId);
         return nil
     }
 
