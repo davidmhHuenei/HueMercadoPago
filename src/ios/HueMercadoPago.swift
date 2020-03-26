70a5f7b5ccbf1aea9188e60a2a737c244155e9a6
David <david.m.hermosilla@gmail.com>
2020-03-26 12:47:46 -0300

    .

--- src/ios/HueMercadoPago.swift
+++ src/ios/HueMercadoPago.swift
@@ -6,6 +6,8 @@ import UIKit
 
 @objc(HueMercadoPago) class HueMercadoPago : CDVPlugin {
 
+    var command: CDVCommandStatus_OK?
+
     @objc(coolMethod:) // Declare your function name.
     func coolMethod(command: CDVInvokedUrlCommand) { // write the function code.
         /* 
@@ -19,9 +21,8 @@ import UIKit
         // Send the function result back to Cordova.
 
         let preferenceId = command.arguments[0]
-
+        self.command = command;
         self.runMercadoPagoCheckoutWithLifecycle(preferenceId)
-        
     }
 
 
@@ -33,63 +34,36 @@ import UIKit
         // 2) Create Checkout reference
         checkout = MercadoPagoCheckout(builder: builder)
         
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let nav1 = UINavigationController()
-        let mainView = ViewController(nibName: nil, bundle: nil) //ViewController = Name of your controller
-        nav1.viewControllers = [mainView]
-        self.window!.rootViewController = nav1
-        self.window?.makeKeyAndVisible()
+        let storyboard: UIStoryboard = UIStoryboard(name: "HomeMP", bundle: nil)
+        //let root = UIApplication.shared.keyWindow?.rootViewController
+        
+        let navController = storyboard.instantiateViewController(withIdentifier: "HomeMP")
+        self.present(navController, animated: true)
+        
         // 3) Start with your navigation controller.
-        checkout?.start(navigationController: nav1, lifeCycleProtocol: self)
+        checkout?.start(navigationController: navController as! UINavigationController, lifeCycleProtocol: self)
     }
-/*
-  - (void)startCheckout:(CDVInvokedUrlCommand*)command {
-    NSString* callbackId = [command callbackId];
-    NSString* publicKey = [[command arguments] objectAtIndex:0];
-    NSString* prefId = [[command arguments] objectAtIndex:1];
-    
-    [MercadoPagoContext setPublicKey:publicKey];
-    
-    if ([[command arguments] objectAtIndex:2]!= (id)[NSNull null]){
-        UIColor *color = [UIColor colorwithHexString:[[command arguments] objectAtIndex:2] alpha:1];
-        [MercadoPagoContext setupPrimaryColor:color complementaryColor:nil];
-    } else {
-        UIColor *color = [UIColor colorwithHexString:MERCADO_PAGO_BASE_COLOR alpha:1];
-        [MercadoPagoContext setupPrimaryColor:color complementaryColor:nil];
-    }
-    if ([[[command arguments] objectAtIndex:3]boolValue]){
-        [MercadoPagoContext setDarkTextColor];
-    }else {
-        [MercadoPagoContext setLightTextColor];
-    }
-    
-    UINavigationController *choFlow =[MPFlowBuilder startCheckoutViewController:prefId callback:^(Payment *payment) {
-        NSString *mppayment = [payment toJSONString];
-        
-        CDVPluginResult* result = [CDVPluginResult
-                                   resultWithStatus:CDVCommandStatus_OK
-                                   messageAsString:mppayment];
-        
-        [self.commandDelegate sendPluginResult:result callbackId:callbackId];
-        
-    }callbackCancel:nil];
-    
-    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-    
-    [rootViewController presentViewController:choFlow animated:YES completion:^{}];
-    
-}*/
 }
 
 // MARK: Optional Lifecycle protocol implementation example.
 extension ViewController: PXLifeCycleProtocol {
-    func finishCheckout() -> ((result: PXResult?) -> Void)? {
-        self.commandDelegate!.send(pluginResult, callbackId: result);
-        return nil
+    public func finishCheckout() -> ((_ payment: PXResult?) -> Void)? {
+        self.dismiss(animated: false)
+        return ({ (_ payment: PXResult?) in
+            let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: self.payment
+            )
+            self.commandDelegate!.send(pluginResult, callbackId: self.command.callbackId);
+        })
     }
 
-    func cancelCheckout() -> ((message) -> Void)? {
-        self.commandDelegate!.send(pluginResult, callbackId: message);
+    func cancelCheckout() -> (() -> Void)? {
+        let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAsString: "Canceled"
+            )
+        self.commandDelegate!.send(pluginResult, callbackId: self.command.callbackId);
         return nil
     }
 
