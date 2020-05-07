e99ea287a0ff4034c8c624a2166147d254761b04
David <david.m.hermosilla@gmail.com>
2020-05-07 15:21:45 -0300

    Fix onActivityResult plugin never called

--- src/android/HueMercadoPago.java
+++ src/android/HueMercadoPago.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.app.Activity;
 
+import com.google.gson.Gson;
 import com.mercadopago.android.px.core.MercadoPagoCheckout;
 import com.mercadopago.android.px.model.Payment;
 import com.mercadopago.android.px.model.exceptions.MercadoPagoError;
@@ -17,11 +19,13 @@ import okhttp3.*;
  * This class echoes a string called from JavaScript.
  */
 public class HueMercadoPago extends CordovaPlugin {
-    private static final int REQUEST_CODE = 1;
+    private static final int REQUEST_CODE = 8091;
     private CallbackContext callbackContext;
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this.callbackContext = callbackContext;
+        cordova.setActivityResultCallback(this);
+
         if (action.equals("coolMethod")) {
             String preferenceId = args.getString(0);
             this.coolMethod(preferenceId);
@@ -35,22 +39,25 @@ public class HueMercadoPago extends CordovaPlugin {
                 .startPayment(this.cordova.getActivity(), REQUEST_CODE);
     }
 
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE) {
             if (resultCode == MercadoPagoCheckout.PAYMENT_RESULT_CODE) {
                 final Payment payment = (Payment) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_PAYMENT_RESULT);
-                this.callbackContext.success(payment.getPaymentStatus());
-                //Done!
+                try{
+                    final PluginResult result = new PluginResult(PluginResult.Status.OK, new JSONObject(new Gson().toJson(payment)));
+                    callbackContext.sendPluginResult(result);
+                }catch(Exception e){
+                    e.printStackTrace();
+                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, e.getMessage()));
+                }
             } else if (resultCode == Activity.RESULT_CANCELED) {
-                if (data != null && data.getExtras() != null
-                        && data.getExtras().containsKey(MercadoPagoCheckout.EXTRA_ERROR)) {
-                    final MercadoPagoError mercadoPagoError =
-                            (MercadoPagoError) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_ERROR);
-                            callbackContext.error(mercadoPagoError.getMessage());
-                    //Resolve error in checkout
+                if (data != null && data.getExtras() != null && data.getExtras().containsKey(MercadoPagoCheckout.EXTRA_ERROR)) {
+                    final MercadoPagoError mercadoPagoError = (MercadoPagoError) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_ERROR);
+                    callbackContext.error(mercadoPagoError.getMessage());
                 } else {
-                    //Resolve canceled checkout
+                    callbackContext.error("Cancelado por usuario");
                 }
             }
         }
